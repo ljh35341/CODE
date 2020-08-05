0fb7ec4bfac59b87482d3c0c9ff06868d540e347
ljh35341 <50003184+ljh35341@users.noreply.github.com>
2020-08-05 11:02:49 +0900

    powershell

---
+++ powershell_youtube_auto.ps1
@@ -0,0 +1,4 @@
+Start-Process -FilePath "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" -ArgumentList "https://pixabay.com/ko/"
+Start-Process -FilePath "C:\Program Files (x86)\oCam\oCam.exe"
+Start-Process -FilePath "C:\Users\user\Desktop\image"
+Start-Process -FilePath "C:\Users\Public\Desktop\PowerPoint 2016" -ArgumentList "PPT.pptm"
